6262a1d3c3d280ad3209db734199beaf902d8131
Christina Lee <chrlee93@gmail.com>
2023-08-09 13:05:59 -0700

    finished tab 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+import "./App.css";
 
-import './App.css'
-
-function App() {
-
+export default function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
+      <SignUpForm token={token} setToken={setToken} />
 
-
-
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Authenticate({ token, setToken }) {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      setSuccessMessage(result.message);
+      setUserData(result.data);
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Authenticate!</h2>
+        {successMessage && <p>{successMessage}</p>}
+        {userData && <p>{userData}</p>}
+        {error && <p>{error}</p>}
+        <button onClick={handleClick}>Authenticate Token</button>
+      </div>
+    </>
+  );
+}
